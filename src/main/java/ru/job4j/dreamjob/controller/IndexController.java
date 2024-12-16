132c5b1c84c779583e0d32c42d3de10b1019f5d1
itlazykin <dzaka89@gmail.com>
2024-12-16 09:16:53 +0300

    1. Генератор HTML [#504838]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -2,6 +2,7 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class IndexController {
     @GetMapping("/index")
