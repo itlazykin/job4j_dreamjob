51a6269c82b514f0d60bd119f980c2d586c6089a
itlazykin <dzaka89@gmail.com>
2024-12-18 09:03:10 +0300

    1. Генератор HTML [#504838 #525577]. Правки по задаче.

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -2,7 +2,9 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/*
+temporary comment
+ */
 @Controller
 public class IndexController {
     @GetMapping("/index")
