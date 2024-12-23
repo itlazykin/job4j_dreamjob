de80fd8a48525abd053ac3cb917ecfdb5b2ab5b4
itlazykin <dzaka89@gmail.com>
2024-12-23 16:40:13 +0300

    var candidate = sql2oCandidateRepository.save(new Candidate(0, "name", "description", creationDate, 1, file.getId()));

--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -4,6 +4,7 @@ import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Vacancy;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,17 +19,17 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     private MemoryVacancyRepository() {
         save(new Vacancy(
-                1, "Intern Java Developer", "Без опыта", true, 1, 0));
+                1, "Intern Java Developer", "Без опыта", LocalDateTime.now(), true, 1, 0));
         save(new Vacancy(
-                2, "Junior Java Developer", "Опыт 1 год", true, 2, 0));
+                2, "Junior Java Developer", "Опыт 1 год", LocalDateTime.now(), true, 2, 0));
         save(new Vacancy(
-                3, "Junior+ Java Developer", "Опыт 2 года", false, 3, 0));
+                3, "Junior+ Java Developer", "Опыт 2 года", LocalDateTime.now(), false, 3, 0));
         save(new Vacancy(
-                4, "Middle Java Developer", "Опыт 3 года", true, 1, 0));
+                4, "Middle Java Developer", "Опыт 3 года", LocalDateTime.now(), true, 1, 0));
         save(new Vacancy(
-                5, "Middle+ Java Developer", "Опыт 4 года", false, 2, 0));
+                5, "Middle+ Java Developer", "Опыт 4 года", LocalDateTime.now(), false, 2, 0));
         save(new Vacancy(
-                6, "Senior Java Developer", "Опыт 6 лет", true, 3, 0));
+                6, "Senior Java Developer", "Опыт 6 лет", LocalDateTime.now(), true, 3, 0));
     }
 
     @Override
@@ -46,14 +47,15 @@ public class MemoryVacancyRepository implements VacancyRepository {
     @Override
     public boolean update(Vacancy vacancy) {
         return vacancies.computeIfPresent(vacancy.getId(), (id, oldVacancy) ->
-            new Vacancy(
-                    oldVacancy.getId(),
-                    vacancy.getTitle(),
-                    vacancy.getDescription(),
-                    vacancy.getVisible(),
-                    vacancy.getCityId(),
-                    vacancy.getFileId()
-            )) != null;
+                new Vacancy(
+                        oldVacancy.getId(),
+                        vacancy.getTitle(),
+                        vacancy.getDescription(),
+                        vacancy.getCreationDate(),
+                        vacancy.getVisible(),
+                        vacancy.getCityId(),
+                        vacancy.getFileId()
+                )) != null;
     }
 
     @Override
